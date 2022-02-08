f659807154c464a00120007e202bfc13ba3cd120
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2022-02-08 14:43:22 -0300

    pedra-papel-tesoura

---
+++ app/src/main/java/com/cursoandroid/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.cursoandroid.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada( "pedra" );
+
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada( "papel" );
+
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada( "tesoura" );
+
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+
+        ImageView imageResultado = findViewById(R.id.imageResultado);
+        TextView textoResultado = findViewById(R.id.textResultado);
+
+        int numero = new Random().nextInt( 3); //0 1 2
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String opcaoApp = opcoes[ numero];
+
+        switch (opcaoApp){
+            case "pedra" :
+                imageResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel" :
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura" :
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if (
+                        (opcaoApp == "tesoura" && opcaoSelecionada == "papel") ||
+                        (opcaoApp == "papel" && opcaoSelecionada == "pedra")||
+                        (opcaoApp == "pedra" && opcaoSelecionada == "tesoura")
+        )
+        {
+            textoResultado.setText("Voce perdeu");
+
+        }else if(
+                         (opcaoSelecionada == "tesoura" && opcaoApp == "papel") ||
+                         (opcaoSelecionada == "papel" && opcaoApp == "pedra")||
+                         (opcaoSelecionada == "pedra" && opcaoApp == "tesoura")
+        ){
+            textoResultado.setText("Voce ganhou");
+
+        }else {
+            textoResultado.setText("Empatamos");
+        }
+
+
+
+        System.out.println("item clicado:" + opcaoApp);
+    }
+
+}
